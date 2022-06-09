c9315b8bffc801de57bc72bcd57cd2b9629e76f1
小涛干饭 <9682800+xiaotao-dry-rice@user.noreply.gitee.com>
2022-06-09 14:50:02 +0800

    创建项目

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    // 添加导入规则
+    requireConfigFile: false
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 关闭组件命名检查
+    'vue/multi-word-component-names': 'off'
+  }
+}
