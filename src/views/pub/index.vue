5bba3c45c66efff6374d5223e96562dfdeb5c349
诗三两 <9903762+poem-three-liang@user.noreply.gitee.com>
2022-06-10 16:51:14 +0800

    酒馆页面

--- src/views/pub/index.vue
+++ src/views/pub/index.vue
@@ -31,9 +31,9 @@
       </div>
 
       <div class="second">
-        <img src="@/pubimg/欠债还钱.png" class="img1" />
+        <img src="@/image/pubimg/欠债还钱.png" class="img1" />
         <span class="span">徐凤年年底睡觉睡觉</span>
-        <img src="@/pubimg/等级8.png" class="img2" />
+        <img src="@/image/pubimg/等级8.png" class="img2" />
         <a class="p">五分钟前</a>
         <div class="zore">零命雷神毕业面板</div>
         <span class="thor"
@@ -49,9 +49,9 @@
       </div>
 
         <div class="second">
-          <img src="@/pubimg/欠债还钱.png" class="img1" />
+          <img src="@/image/pubimg/欠债还钱.png" class="img1" />
           <span class="span">徐凤年年底睡觉睡觉</span>
-          <img src="@/pubimg/等级8.png" class="img2" />
+          <img src="@/image/pubimg/等级8.png" class="img2" />
           <a class="p">五分钟前</a>
           <div class="zore">零命雷神毕业面板</div>
           <span class="thor"
