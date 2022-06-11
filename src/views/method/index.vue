4e50fec023322b8c232911f9b0b493055822f831
诗三两 <9903762+poem-three-liang@user.noreply.gitee.com>
2022-06-11 14:56:58 +0800

    攻略页面

--- src/views/method/index.vue
+++ src/views/method/index.vue
@@ -1,7 +1,244 @@
 <template>
-    <div>
-        这是攻略页面
+  <div class="edition a1">
+    <div class="forum">
+      <div class="layout">
+        <div class="forum-heard">
+          <div class="artice">
+            <div class="type-st">
+              <ul>
+                <li>最新发帖</li>
+                <li class="type-tab">
+                  <span class="type-item">最新回复</span>
+                </li>
+                <li class="type-tab">热门</li>
+                <li class="type-tab">攻略分类</li>
+              </ul>
+            </div>
+          </div>
+          <div class="defalut">
+            <el-button class="defal-botn">
+              <i class="el-icon-edit"></i>
+              <span>发布帖子</span>
+            </el-button>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="toal">
+      <div class="filter-first">
+        <div class="first">
+          <span>置顶</span>
+          <a>【攻略区版规】将会移除求助、讨论、反馈贴等非攻略贴</a>
+        </div>
+        <div class="first">
+          <span>置顶</span>
+          <a>【攻略索引】角色攻略、世界任务、材料采集、BOSS 持更</a>
+        </div>
+        <div class="first">
+          <span>置顶</span>
+          <a>2.7版本｜夜兰、魈、若水弓评测等 攻略合集「持续更新」</a>
+        </div>
+      </div>
+      <!-- 发布的消息 -->
+      <div class="second">
+        <img src="@/image/pubimg/欠债还钱.png" class="img1" />
+        <span class="span">徐凤年年底睡觉睡觉</span>
+        <img src="@/image/pubimg/等级8.png" class="img2" />
+        <a class="p">五分钟前</a>
+        <div class="zore">零命雷神毕业面板</div>
+        <span class="thor"
+          >零命雷神这样算毕业吗 感觉已经到极限了 再往前提太难了
+          零命和两命的差距真的好大</span
+        >
+        <div class="img3">
+          <div class="cobo"></div>
+          <div class="eight"></div>
+          <div class="fold"></div>
+        </div>
+        <div class="page">
+          <div class="second-card">
+            <i class="el-icon-view"></i>
+            <span>4万+</span>
+          </div>
+          <div class="second-data">
+            <i class="el-icon-chat-dot-round"></i>
+            <span>3134</span>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div class="second">
+        <img src="@/image/pubimg/欠债还钱.png" class="img1" />
+        <span class="span">徐凤年年底睡觉睡觉</span>
+        <img src="@/image/pubimg/等级8.png" class="img2" />
+        <a class="p">五分钟前</a>
+        <div class="zore">零命雷神毕业面板</div>
+        <span class="thor"
+          >零命雷神这样算毕业吗 感觉已经到极限了 再往前提太难了
+          零命和两命的差距真的好大</span
+        >
+        <div class="img3">
+          <div class="cobo"></div>
+          <div class="eight"></div>
+          <div class="fold"></div>
+        </div>
+        <div class="page">
+          <div class="second-card">
+            <i class="el-icon-view"></i>
+            <span>4万+</span>
+          </div>
+          <div class="second-data">
+            <i class="el-icon-chat-dot-round"></i>
+            <span>3134</span>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div class="second">
+        <img src="@/image/method/三神.png" class="img1" />
+        <span class="span">看三神给钱</span>
+        <img src="@/image/method/等级11.png" class="img2" />
+        <a class="p">16分钟前</a>
+        <div class="zore">零命雷神毕业面板</div>
+        <span class="thor"
+          >零命雷神这样算毕业吗 感觉已经到极限了 再往前提太难了
+          零命和两命的差距真的好大</span
+        >
+        <div class="img3">
+          <div class="cobo2"></div>
+          <div class="eight"></div>
+          <div class="fold"></div>
+        </div>
+         <div class="page">
+          <div class="second-card">
+            <i class="el-icon-view"></i>
+            <span>4万+</span>
+          </div>
+          <div class="second-data">
+            <i class="el-icon-chat-dot-round"></i>
+            <span>3134</span>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+    <div class="code">
+      <!-- 搜素框 -->
+      <div class="guide">
+        <div class="guide-searh">
+          <el-input />
+        </div>
+          <div class="guide-submit">
+          <button type="submit" class="defult">搜素</button>
+        </div>
+      </div>
+      <!-- 扫码下载 -->
+      <div class="sem">
+        <div class="myh">
+          <img src="@/image/二维码.png" />
+        </div>
+        <div class="qrcode">
+          <p class="qrcode-titil">扫码下载米游社App</p>
+          <p class="qrcode-suder">了解提瓦特最新资讯</p>
+        </div>
+      </div>
+      <!-- 官方资讯 -->
+      <div class="side">
+        <div class="side-uster">
+          <h2>官方资讯</h2>
+          <a>更多</a>
+        </div>
+        <div class="side-body">
+          <div class="body-op">
+            <p class="body-list">米游币抽抽乐-原神专场现已开启！</p>
+          </div>
+        </div>
+        <div class="list-banner">
+          <div class="banner-info">
+            <span>活动</span>
+            <p>【转发抽奖】丽莎生日活动开启！</p>
+          </div>
+        </div>
+
+        <div class="list-banner">
+          <div class="banner-info">
+            <span>资讯</span>
+            <p>【已开奖】「流风眷堇庭」2.6版本攻略征集活动获奖名单</p>
+          </div>
+        </div>
+
+        <div class="list-banner">
+          <div class="banner-info">
+            <span>资讯</span>
+            <p>【已开奖】「流风眷堇庭」2.6版本攻略征集活动获奖名单</p>
+          </div>
+        </div>
+
+        <div class="list-banner">
+          <div class="banner-info">
+            <span>公告</span>
+            <p>2.7版本游戏问题集中反馈-6月9日更新</p>
+          </div>
+        </div>
+
+        <div class="list-banner">
+          <div class="banner-info">
+            <span>公告</span>
+            <p>「荒梦藏虞渊」2.7版本更新说明</p>
+          </div>
+        </div>
+      </div>
+      <!-- 热门话题 -->
+      <div class="side">
+        <div class="side-uster">
+          <h2>热门话题</h2>
+          <a>更多</a>
+        </div>
+        <div class="side-body">
+          <div class="body-topic">
+            <div class="topic-card">
+              <img src="@/image/method/探索解谜.jpg" />
+            </div>
+            <div class="card-info">
+              <p>探索解谜</p>
+            </div>
+          </div>
+          <div class="body-topic">
+            <div class="topic-card">
+              <img src="@/image/method/角色攻略.jpg" />
+            </div>
+            <div class="card-info">
+              <p>角色攻略</p>
+            </div>
+          </div>
+          <div class="body-topic">
+            <div class="topic-card">
+              <img src="@/image/method/其他攻略.jpg" />
+            </div>
+            <div class="card-info">
+              <p>其他攻略</p>
+            </div>
+          </div>
+          <div class="body-topic">
+            <div class="topic-card">
+              <img src="@/image/method/萌新指南.jpg" />
+            </div>
+            <div class="card-info">
+              <p>萌新指南</p>
+            </div>
+          </div>
+          <div class="body-topic">
+            <div class="topic-card">
+              <img src="@/image/method/讨伐手册.jpg" />
+            </div>
+            <div class="card-info">
+              <p>讨伐手册</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +247,6 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="sass">
 
 </style>
