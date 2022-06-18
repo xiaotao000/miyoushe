d2bd78aa85f7b76bd0303839abb20ea5ba569a10
诗三两 <9903762+poem-three-liang@user.noreply.gitee.com>
2022-06-18 09:43:09 +0800

    渲染用户发帖

---
+++ src/api/release.js
@@ -0,0 +1,4 @@
+import request from "@/utils/request"
+
+// 获取用户发布帖子
+export const reWgyh = () => request({ method:'get',url:'/my/user/article'})
--- src/views/personal/persook/ginhan/index.vue
+++ src/views/personal/persook/ginhan/index.vue
@@ -3,16 +3,64 @@
     <div class="nmh">
       <div class="nm">我的发帖</div>
     </div>
-    <div class="wgFy">
-      <img class="npmIng" src="https://bbs.mihoyo.com/_nuxt/img/noData.a3d7135.png" alt="" width="200px">
-      <p>暂时没有帖子哟</p>
+      <div class="second" v-for="item in card" :key="item.id">
+        <a class="p">{{item.time}}</a>
+        <div class="zore">{{item.title}}</div>
+        <span class="thor"
+          >{{item.introduce}}</span>
+        <!-- 发布的图片 -->
+        <div class="img3">
+          <div class="cobo" v-for="img,i in item.cover" :key="i">
+            <img :src="`${'http://mys.flycran.xyz'+ img}`" alt="">
+          </div>
+        </div>
+        <div class="topi">
+          <!-- 标签 -->
+          <div class="topces">
+            <div class="topc">
+              <span>冒险家·集结</span>
+            </div>
+            <div class="topc">
+              <span>每日一水</span>
+            </div>
+          </div>
+          <!-- 小图标 -->
+        <div class="page">
+          <div class="second-card">
+            <i class="el-icon-view"></i>
+            <span>{{item.browse}}</span>
+          </div>
+          <div class="second-data">
+            <i class="el-icon-chat-dot-round"></i>
+            <span>3134</span>
+          </div>
+          <div class="second-data">
+            <i class="iconfont iconfont icon-dianzan"></i>
+            <span>126</span>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
+import  { reWgyh} from '@/api/release'
 export default {
-
+data(){
+  return{
+    card:[]
+  }
+},
+mounted(){
+  this.getList()
+},
+methods:{
+    async getList (){
+      const add = await reWgyh()
+      this.card = add
+}
+}
 }
 </script>
 
